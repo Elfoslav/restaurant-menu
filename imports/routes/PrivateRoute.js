34da1fd7a7bc4b21a81471b05f247b4abec81427
elfoslav <tom.hromnik@gmail.com>
2019-06-17 21:55:47 +0200

    Prevent unwanted redirect on secured routes

--- imports/routes/PrivateRoute.js
+++ imports/routes/PrivateRoute.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
 
 import DefaultLayout from './DefaultLayout';
 
 function PrivateRoute({ component: Component, ...rest }) {
-  const isLoggedIn = Meteor.user();
+  const [isLoggedIn, setIsLoggedIn] = useState(Meteor.userId());
   return (
     <Route
       {...rest}
